90fc41827c65b5cf7a5f6b1d44e0c7000033fd52
InJoon-L <dlswns619@naver.com>
2022-10-18 11:35:57 +0900

    ts-useRef

--- src/components/InputSample.tsx
+++ src/components/InputSample.tsx
@@ -1,22 +1,46 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+
+interface inputSampleText {
+	name: string;
+	nickname: string;
+}
 
 const InputSample = () => {
-	const [text, setText] = useState<string>('');
+	const [text, setText] = useState<inputSampleText>({
+		name: '',
+		nickname: ''
+	});
+
+	const nameInput = useRef<HTMLInputElement>(null);
+
+	const { name, nickname } = text; // 비구조화 할당을 통해 값 추출
 
 	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setText(e.target.value);
+		const { value, name } = e.target; // 우선 e.target에서 name과 value를 추출
+		
+		setText({
+			...text, // 기존의 text객체를 복사한 뒤
+			[name]: value // name 키를 가진 값을 value로 설정
+		});
 	}
 
 	const onReset = () => {
-		setText('');
+		setText({
+			name: '',
+			nickname: ''
+		});
+
+		nameInput.current?.focus();
 	}
 
 	return (
 		<div>
-			<input onChange={onChange} value={text} type="text" />
+			<input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
 			<button onClick={onReset}>초기화</button>
 			<div>
-				<b>값: {text}</b>
+				<b>값: </b>
+				{name} ({nickname})
 			</div>
 		</div>
 	);
